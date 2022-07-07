44b63ed6b47e2dbaf53e8694a9f6e342e493340b
Joshua <joshjoshuap1@gmail.com>
2022-07-07 15:57:32 +0800

    chore: add header

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,17 @@
 const HomePage = () => {
   return (
     <>
-      <h1>Hello</h1>
+      <header>
+          <h1 class="header-title"> WEB DEVELOPER ROADMAP </h1>
+          <p class="header-description">
+            My Personal roadmap and guide to learn and study web development. Contains Links,
+            websites and sample projects
+          </p>
+    
+        <a href="#" class="header-btn">
+          Lets Study
+        </a>
+      </header>
     </>
   );
 };
