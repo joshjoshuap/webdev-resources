32666cbe8dc5363bc093fb1c49aa44b44d5b5822
Joshua <joshjoshuap1@gmail.com>
2022-07-08 16:47:01 +0800

    chore: add bootstrap, tailwindcss, reactjs in frontend development

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,9 @@ import Image from 'next/image';
 import HTMLLogo from '../public/images/html5-logo.svg';
 import CSSLogo from '../public//images/css3-logo.svg';
 import JSLogo from '../public//images/javascript-logo.svg';
+import BootstrapLogo from '../public/images/bootstrap-logo.svg';
+import TailwindLogo from '../public/images/tailwindcss-logo.svg';
+import ReactLogo from '../public/images/reactjs-logo.svg';
 
 const HomePage = () => {
   return (
@@ -34,6 +37,7 @@ const HomePage = () => {
           TOOLS
         </a>
       </nav>
+
       <section id="frontend">
         <h1 class="section-title">FRONT END DEVELOPMENT</h1>
         <div class="card-container">
@@ -105,8 +109,8 @@ const HomePage = () => {
             </div>
           </div>
 
-           {/* Javascript */}
-           <div class="card-item">
+          {/* Javascript */}
+          <div class="card-item">
             <div class="card-image">
               <Image src={JSLogo} height={80} width={80} alt="HTML 5 Logo" />
             </div>
@@ -136,8 +140,150 @@ const HomePage = () => {
                   </a>
                 </li>
                 <li>
-                  <a href="https://github.com/rwaldron/idiomatic.js" target="_blank" class="card-link">
+                  <a
+                    href="https://github.com/rwaldron/idiomatic.js"
+                    target="_blank"
+                    class="card-link">
                     Idiomatic Javascript
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+
+          {/* Bootstrap */}
+          <div class="card-item">
+            <div class="card-image">
+              <Image src={BootstrapLogo} height={80} width={80} alt="HTML 5 Logo" />
+            </div>
+            <div class="card-description">
+              <ul>
+                <li>
+                  <a
+                    href="https://getbootstrap.com/docs/5.2/getting-started/introduction/"
+                    target="_blank"
+                    class="card-link">
+                    Bootstrap 5.2
+                  </a>
+                </li>
+                <li>
+                  <a href="https://bootsnipp.com/" target="_blank" class="card-link">
+                    Bootsnipp Template
+                  </a>
+                </li>
+              </ul>
+              <ul>
+                <li>
+                  <a
+                    href="https://hackerthemes.com/bootstrap-tools/"
+                    target="_blank"
+                    class="card-link">
+                    Bootstrap Tools
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://bootstrap-cheatsheet.themeselection.com/"
+                    target="_blank"
+                    class="card-link">
+                    Bootstrap 5 Cheatsheet
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+
+          {/* Tailwind CSS */}
+          <div class="card-item">
+            <div class="card-image">
+              <Image src={TailwindLogo} height={80} width={80} alt="HTML 5 Logo" />
+            </div>
+            <div class="card-description">
+              <ul>
+                <li>
+                  <a
+                    href="https://tailwindcss.com/"
+                    target="_blank"
+                    class="card-link">
+                    Tailwind CSS
+                  </a>
+                </li>
+                <li>
+                  <a href="https://www.tailwindtoolbox.com/tools" target="_blank" class="card-link">
+                   Tailwind Toolbox
+                  </a>
+                </li>
+              </ul>
+              <ul>
+                <li>
+                  <a
+                    href="https://postsrc.com/components"
+                    target="_blank"
+                    class="card-link">
+                    Tailwind Components
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://bestoftailwind.com/t/tools"
+                    target="_blank"
+                    class="card-link">
+                    Tailwind Tools
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+
+           {/* React JS */}
+           <div class="card-item">
+            <div class="card-image">
+              <Image src={ReactLogo} height={80} width={80} alt="HTML 5 Logo" />
+            </div>
+            <div class="card-description">
+              <ul>
+                <li>
+                  <a
+                    href="https://reactjs.org/docs/getting-started.html"
+                    target="_blank"
+                    class="card-link">
+                    React JS
+                  </a>
+                </li>
+                <li>
+                  <a href="https://mui.com/material-ui/getting-started/overview/" target="_blank" class="card-link">
+                   Material UI
+                  </a>
+                </li>
+                <li>
+                  <a href="https://chakra-ui.com/getting-started" target="_blank" class="card-link">
+                   Chakra UI
+                  </a>
+                </li>
+              </ul>
+              <ul>
+              <li>
+                  <a
+                    href="https://redux.js.org/introduction/getting-started"
+                    target="_blank"
+                    class="card-link">
+                    Redux
+                  </a>
+                </li>
+              <li>
+                  <a
+                    href="https://reactrouter.com/docs/en/v6"
+                    target="_blank"
+                    class="card-link">
+                    React Router
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://devhints.io/react"
+                    target="_blank"
+                    class="card-link">
+                    ReactJS Cheatsheet
                   </a>
                 </li>
               </ul>
