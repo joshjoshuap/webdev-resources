b7f9d79b96bf82247afdc05b2de1b29869381d69
Joshua <joshjoshuap1@gmail.com>
2022-07-08 16:59:58 +0800

    chore: change title and description

--- pages/index.js
+++ pages/index.js
@@ -10,9 +10,9 @@ const HomePage = () => {
   return (
     <>
       <header>
-        <h1 class="header-title"> WEB DEVELOPER ROADMAP </h1>
+        <h1 class="header-title"> WEB DEVELOPER RESOURCES </h1>
         <p class="header-description">
-          My Personal roadmap and guide to learn and study web development. Contains Links, websites
+          My Personal resources and source to learn and study web development. Contains Links, websites
           and sample projects
         </p>
 
