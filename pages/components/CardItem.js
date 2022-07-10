773a751f9f5ed3d40d49b2b7ef50377e3c6672fb
Joshua <joshjoshuap1@gmail.com>
2022-07-10 18:27:32 +0800

    refractor: refractor using components and props

---
+++ pages/components/CardItem.js
@@ -0,0 +1,26 @@
+import Image from 'next/image';
+
+const CardInfo = (info) => {
+  return (
+    <span class="card-link">
+      <a href={info.link} target="_blank">
+        {info.title}
+      </a>
+    </span>
+  );
+};
+
+const CardItem = (props) => {
+  return (
+    <>
+      <div class="card-item">
+        <div class="card-image">
+          <Image src={props.image} height={80} width={80} alt="HTML 5 Icon" />
+        </div>
+        <div class="card-info">{props.info.map(CardInfo)}</div>
+      </div>
+    </>
+  );
+};
+
+export default CardItem;
--- pages/sections/Backend.js
+++ pages/sections/Backend.js
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import CardItem from '../components/CardItem';
 import NodeIcon from '../../public/images/nodejs-icon.svg';
 import ExpressIcon from '../../public/images/expressjs-icon.svg';
 import MongoDBIcon from '../../public/images/mongodb-icon.svg';
@@ -7,97 +8,39 @@ const Backend = () => {
   return (
     <section id="backend">
       <h1 class="section-title">BACK END DEVELOPMENT</h1>
-      <div class="card-container">
+      <div class="container">
         {/* NodeJS */}
-        <div class="card-item">
-          <div class="card-image">
-            <Image src={NodeIcon} height={80} width={80} alt="HTML 5 Icon" />
-          </div>
-          <div class="card-description">
-            <ul>
-              <li>
-                <a
-                  href="https://nodejs.org/dist/latest-v16.x/docs/api/"
-                  target="_blank"
-                  class="card-link">
-                  Node JS v16
-                </a>
-              </li>
-              <li>
-                <a href="https://www.npmjs.com/" target="_blank" class="card-link">
-                  NPM
-                </a>
-              </li>
-            </ul>
-            <ul>
-              <li>
-                <a
-                  href="https://github.com/goldbergyoni/nodebestpractices?utm_source=mybridge&utm_medium=blog&utm_campaign=read_more"
-                  target="_blank"
-                  class="card-link">
-                  Node Best Practices
-                </a>
-              </li>
-              <li>
-                <a
-                  href="https://github.com/sindresorhus/awesome-nodejs"
-                  target="_blank"
-                  class="card-link">
-                  Awesome NodeJS
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
+        <CardItem
+          image={NodeIcon}
+          width={200}
+          info={[
+            { link: 'https://nodejs.org/dist/latest-v16.x/docs/api/', title: 'Node JS v16' },
+            { link: 'https://www.npmjs.com/', title: 'NPM' },
+            {
+              link: 'https://github.com/goldbergyoni/nodebestpractices?utm_source=mybridge&utm_medium=blog&utm_campaign=read_more',
+              title: 'Node Best Practices',
+            },
+            { link: 'https://github.com/sindresorhus/awesome-nodejs', title: ' Awesome NodeJS' },
+          ]}
+        />
 
-        {/* Express JS */}
-        <div class="card-item">
-          <div class="card-image">
-            <Image src={ExpressIcon} height={80} width={150} alt="HTML 5 Icon" />
-          </div>
-          <div class="card-description">
-            <ul>
-              <li>
-                <a href="https://expressjs.com/en/4x/api.html" target="_blank" class="card-link">
-                  Express JS
-                </a>
-              </li>
-            </ul>
-            <ul>
-              <li>
-                <a href="https://restfulapi.net/" target="_blank" class="card-link">
-                  Rest API
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
+        {/* ExpressJS */}
+        <CardItem
+          image={ExpressIcon}
+          info={[
+            { link: 'https://expressjs.com/en/4x/api.html', title: 'Express JS' },
+            { link: 'https://restfulapi.net/', title: 'Rest API' },
+          ]}
+        />
 
         {/* MongoDB */}
-        <div class="card-item">
-          <div class="card-image">
-            <Image src={MongoDBIcon} height={80} width={150} alt="HTML 5 Icon" />
-          </div>
-          <div class="card-description">
-            <ul>
-              <li>
-                <a
-                  href="https://www.mongodb.com/docs/manual/introduction/l"
-                  target="_blank"
-                  class="card-link">
-                  MongoDB
-                </a>
-              </li>
-            </ul>
-            <ul>
-              <li>
-                <a href="https://mongoosejs.com/docs/guide.html" target="_blank" class="card-link">
-                  Mongoose
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
+        <CardItem
+          image={MongoDBIcon}
+          info={[
+            { link: 'https://www.mongodb.com/docs/manual/introduction/l', title: 'MongoDB' },
+            { link: 'https://mongoosejs.com/docs/guide.html', title: 'Mongoose' },
+          ]}
+        />
       </div>
     </section>
   );
