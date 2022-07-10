746b314fd779792c77c2e0ffaa634fc473411fa4
Joshua <joshjoshuap1@gmail.com>
2022-07-10 18:44:02 +0800

    fix: fixing map loop compatibility

--- pages/components/CardItem.js
+++ pages/components/CardItem.js
@@ -13,13 +13,14 @@ const CardInfo = (info) => {
 };
 
 const CardItem = (props) => {
+  let infos = props.info;
   return (
     <>
       <div class="card-item">
         <div class="card-image">
           <Image src={props.image} height={80} width={80} alt="HTML 5 Icon" />
         </div>
-        <div class="card-info">{props.info.map(CardInfo)}</div>
+        <div class="card-info">{infos.map(CardInfo)}</div>
       </div>
     </>
   );
