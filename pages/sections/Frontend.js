4d78ef95c67f68680a123b74b3be1fa8b5bfa9be
Joshua <joshjoshuap1@gmail.com>
2022-07-10 18:30:15 +0800

    refractor: repleace with components and props

--- pages/sections/Frontend.js
+++ pages/sections/Frontend.js
@@ -1,4 +1,4 @@
-import Image from 'next/image';
+import CardItem from '../components/CardItem';
 import HTMLIcon from '../../public/images/html5-icon.svg';
 import CSSIcon from '../../public//images/css3-icon.svg';
 import JSIcon from '../../public//images/javascript-icon.svg';
@@ -10,243 +10,81 @@ const FrontEnd = () => {
   return (
     <section id="frontend">
       <h1 class="section-title">FRONT END DEVELOPMENT</h1>
-      <div class="card-container">
+      <div class="container">
         {/* HTML */}
-        <div class="card-item">
-          <div class="card-image">
-            <Image src={HTMLIcon} height={80} width={80} alt="HTML 5 Icon" />
-          </div>
-          <div class="card-description">
-            <ul>
-              <li>
-                <a
-                  href="https://developer.mozilla.org/en-US/docs/Web/HTML"
-                  target="_blank"
-                  class="card-link">
-                  MDN Web HTML
-                </a>
-              </li>
-            </ul>
-            <ul>
-              <li>
-                <a
-                  href="https://www.w3schools.com/html/default.asp"
-                  target="_blank"
-                  class="card-link">
-                  W3School HTML
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
+        <CardItem
+          image={HTMLIcon}
+          info={[
+            { link: 'https://developer.mozilla.org/en-US/docs/Web/HTML', title: 'MDN Web HTML' },
+            { link: 'https://www.w3schools.com/html/default.asp', title: 'W3School HTML' },
+          ]}
+        />
 
         {/* CSS */}
-        <div class="card-item">
-          <div class="card-image">
-            <Image src={CSSIcon} height={80} width={80} alt="HTML 5 Icon" />
-          </div>
-          <div class="card-description">
-            <ul>
-              <li>
-                <a
-                  href="https://developer.mozilla.org/en-US/docs/Web/CSS"
-                  target="_blank"
-                  class="card-link">
-                  MDN Web CSS
-                </a>
-              </li>
-              <li>
-                <a href="https://cssreference.io/" target="_blank" class="card-link">
-                  CSS Reference
-                </a>
-              </li>
-            </ul>
-            <ul>
-              <li>
-                <a
-                  href="https://www.w3schools.com/css/default.asp"
-                  target="_blank"
-                  class="card-link">
-                  W3School CSS
-                </a>
-              </li>
-              <li>
-                <a href="https://css-tricks.com/" target="_blank" class="card-link">
-                  CSS Tricks
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
+        <CardItem
+          image={CSSIcon}
+          info={[
+            { link: 'https://developer.mozilla.org/en-US/docs/Web/CSS', title: 'MDN Web CSS' },
+            { link: 'https://cssreference.io/', title: 'CSS Reference' },
+            { link: 'https://www.w3schools.com/css/default.asp', title: 'W3School CSS' },
+            { link: 'https://css-tricks.com/', title: 'CSS Tricks' },
+          ]}
+        />
 
         {/* Javascript */}
-        <div class="card-item">
-          <div class="card-image">
-            <Image src={JSIcon} height={80} width={80} alt="HTML 5 Icon" />
-          </div>
-          <div class="card-description">
-            <ul>
-              <li>
-                <a
-                  href="https://developer.mozilla.org/en-US/docs/Web/JavaScript"
-                  target="_blank"
-                  class="card-link">
-                  MDN Web Javascript
-                </a>
-              </li>
-              <li>
-                <a href="https://eloquentjavascript.net/" target="_blank" class="card-link">
-                  Eloquent Javascript
-                </a>
-              </li>
-            </ul>
-            <ul>
-              <li>
-                <a
-                  href="https://www.w3schools.com/js/default.asp"
-                  target="_blank"
-                  class="card-link">
-                  W3School Javascript
-                </a>
-              </li>
-              <li>
-                <a
-                  href="https://github.com/rwaldron/idiomatic.js"
-                  target="_blank"
-                  class="card-link">
-                  Idiomatic Javascript
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
+        <CardItem
+          image={JSIcon}
+          info={[
+            {
+              link: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript',
+              title: 'MDN Web Javascript',
+            },
+            { link: 'https://eloquentjavascript.net/', title: 'Eloquent Javascript' },
+            { link: 'https://www.w3schools.com/js/default.asp', title: 'W3School Javascript' },
+            { link: 'https://github.com/rwaldron/idiomatic.js', title: 'Idiomatic Javascript' },
+          ]}
+        />
 
         {/* Bootstrap */}
-        <div class="card-item">
-          <div class="card-image">
-            <Image src={BootstrapIcon} height={80} width={80} alt="HTML 5 Icon" />
-          </div>
-          <div class="card-description">
-            <ul>
-              <li>
-                <a
-                  href="https://getbootstrap.com/docs/5.2/getting-started/introduction/"
-                  target="_blank"
-                  class="card-link">
-                  Bootstrap 5.2
-                </a>
-              </li>
-              <li>
-                <a href="https://bootsnipp.com/" target="_blank" class="card-link">
-                  Bootsnipp Template
-                </a>
-              </li>
-            </ul>
-            <ul>
-              <li>
-                <a
-                  href="https://hackerthemes.com/bootstrap-tools/"
-                  target="_blank"
-                  class="card-link">
-                  Bootstrap Tools
-                </a>
-              </li>
-              <li>
-                <a
-                  href="https://bootstrap-cheatsheet.themeselection.com/"
-                  target="_blank"
-                  class="card-link">
-                  Bootstrap 5 Cheatsheet
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
+        <CardItem
+          image={BootstrapIcon}
+          info={[
+            {
+              link: 'https://getbootstrap.com/docs/5.2/getting-started/introduction/',
+              title: 'Bootstrap 5.2',
+            },
+            { link: 'https://bootsnipp.com/', title: 'Bootsnipp Template' },
+            { link: 'https://hackerthemes.com/bootstrap-tools/', title: 'Bootstrap Tools' },
+            {
+              link: 'https://bootstrap-cheatsheet.themeselection.com/',
+              title: '  Bootstrap 5 Cheatsheet',
+            },
+          ]}
+        />
 
         {/* Tailwind CSS */}
-        <div class="card-item">
-          <div class="card-image">
-            <Image src={TailwindIcon} height={80} width={80} alt="HTML 5 Icon" />
-          </div>
-          <div class="card-description">
-            <ul>
-              <li>
-                <a href="https://tailwindcss.com/" target="_blank" class="card-link">
-                  Tailwind CSS
-                </a>
-              </li>
-              <li>
-                <a href="https://www.tailwindtoolbox.com/tools" target="_blank" class="card-link">
-                  Tailwind Toolbox
-                </a>
-              </li>
-            </ul>
-            <ul>
-              <li>
-                <a href="https://postsrc.com/components" target="_blank" class="card-link">
-                  Tailwind Components
-                </a>
-              </li>
-              <li>
-                <a href="https://bestoftailwind.com/t/tools" target="_blank" class="card-link">
-                  Tailwind Tools
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
+        <CardItem
+          image={TailwindIcon}
+          info={[
+            { link: 'https://tailwindcss.com/', title: 'Tailwind CSS' },
+            { link: 'https://www.tailwindtoolbox.com/tools', title: 'Tailwind Toolbox' },
+            { link: 'https://postsrc.com/components', title: 'Tailwind Components' },
+            { link: 'https://bestoftailwind.com/t/tools', title: 'Tailwind Tools' },
+          ]}
+        />
 
-        {/* React JS */}
-        <div class="card-item">
-          <div class="card-image">
-            <Image src={ReactIcon} height={80} width={80} alt="HTML 5 Icon" />
-          </div>
-          <div class="card-description">
-            <ul>
-              <li>
-                <a
-                  href="https://reactjs.org/docs/getting-started.html"
-                  target="_blank"
-                  class="card-link">
-                  React JS
-                </a>
-              </li>
-              <li>
-                <a
-                  href="https://mui.com/material-ui/getting-started/overview/"
-                  target="_blank"
-                  class="card-link">
-                  Material UI
-                </a>
-              </li>
-              <li>
-                <a href="https://chakra-ui.com/getting-started" target="_blank" class="card-link">
-                  Chakra UI
-                </a>
-              </li>
-            </ul>
-            <ul>
-              <li>
-                <a
-                  href="https://redux.js.org/introduction/getting-started"
-                  target="_blank"
-                  class="card-link">
-                  Redux
-                </a>
-              </li>
-              <li>
-                <a href="https://reactrouter.com/docs/en/v6" target="_blank" class="card-link">
-                  React Router
-                </a>
-              </li>
-              <li>
-                <a href="https://devhints.io/react" target="_blank" class="card-link">
-                  ReactJS Cheatsheet
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
+        {/* ReactJS */}
+        <CardItem
+          image={ReactIcon}
+          info={[
+            { link: 'https://reactjs.org/docs/getting-started.html', title: 'React JS' },
+            { link: 'https://mui.com/material-ui/getting-started/overview/', title: 'Material UI' },
+            { link: 'https://chakra-ui.com/getting-started', title: 'Chakra UI' },
+            { link: 'https://redux.js.org/introduction/getting-started', title: 'Redux' },
+            { link: 'https://reactrouter.com/docs/en/v6', title: 'React Router' },
+            { link: 'https://devhints.io/react', title: 'ReactJS Cheatsheet' },
+          ]}
+        />
       </div>
     </section>
   );
