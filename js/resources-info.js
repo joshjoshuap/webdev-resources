ecbb9d44bf65cb00756d5bb97ded572ee5d7bad2
Joshua <joshjoshuap1@gmail.com>
2022-07-13 15:54:55 +0800

    refractor: refractor backend section

--- js/resources-info.js
+++ js/resources-info.js
@@ -65,6 +65,34 @@ const resourcesInfo = [
         ],
       },
     },
+    backEndResources: {
+      NodeJS: {
+        title: 'NodeJS',
+        item: [
+          { text: 'Node JS v16', link: 'https://nodejs.org/dist/latest-v16.x/docs/api/' },
+          { text: 'NPM', link: 'https://www.npmjs.com/' },
+          {
+            text: 'Node Best Practices',
+            link: 'https://github.com/goldbergyoni/nodebestpractices?utm_source=mybridge&utm_medium=blog&utm_campaign=read_more',
+          },
+          { text: ' Awesome NodeJS', link: 'https://github.com/sindresorhus/awesome-nodejs' },
+        ],
+      },
+      ExpressJS: {
+        title: 'ExpressJS',
+        item: [
+          { text: 'Express JS', link: 'https://expressjs.com/en/4x/api.html' },
+          { text: 'Rest API', link: 'https://restfulapi.net/' },
+        ],
+      },
+      MongoDB: {
+        title: 'MongoDB',
+        item: [
+          { text: 'MongoDB', link: 'https://www.mongodb.com/docs/manual/introduction/l' },
+          { text: 'Mongoose', link: 'https://mongoosejs.com/docs/guide.html' },
+        ],
+      },
+    },
   },
 ];
 
--- pages/sections/Backend.js
+++ pages/sections/Backend.js
@@ -1,3 +1,4 @@
+import { resourcesInfo } from '/js/resources-info.js';
 import Image from 'next/image';
 import CardItem from '/components/CardItem';
 import NodeIcon from '/public/images/nodejs-icon.svg';
@@ -5,43 +6,25 @@ import ExpressIcon from '/public/images/expressjs-icon.svg';
 import MongoDBIcon from '/public/images/mongodb-icon.svg';
 
 const Backend = () => {
+  let backEndInfo = resourcesInfo[0].backEndResources;
   return (
     <section id="backend">
       <h1 className="section-title">BACK END DEVELOPMENT</h1>
       <div className="card-container">
-        {/* NodeJS */}
         <CardItem
           image={NodeIcon}
-          title="NodeJS"
-          info={[
-            { link: 'https://nodejs.org/dist/latest-v16.x/docs/api/', text: 'Node JS v16' },
-            { link: 'https://www.npmjs.com/', text: 'NPM' },
-            {
-              link: 'https://github.com/goldbergyoni/nodebestpractices?utm_source=mybridge&utm_medium=blog&utm_campaign=read_more',
-              text: 'Node Best Practices',
-            },
-            { link: 'https://github.com/sindresorhus/awesome-nodejs', text: ' Awesome NodeJS' },
-          ]}
+          title={backEndInfo.NodeJS.title}
+          info={backEndInfo.NodeJS.item}
         />
-
-        {/* ExpressJS */}
         <CardItem
           image={ExpressIcon}
-          title="ExpressJS"
-          info={[
-            { link: 'https://expressjs.com/en/4x/api.html', text: 'Express JS' },
-            { link: 'https://restfulapi.net/', text: 'Rest API' },
-          ]}
+          title={backEndInfo.ExpressJS.title}
+          info={backEndInfo.ExpressJS.item}
         />
-
-        {/* MongoDB */}
         <CardItem
           image={MongoDBIcon}
-          title="MongoDB"
-          info={[
-            { link: 'https://www.mongodb.com/docs/manual/introduction/l', text: 'MongoDB' },
-            { link: 'https://mongoosejs.com/docs/guide.html', text: 'Mongoose' },
-          ]}
+          title={backEndInfo.MongoDB.title}
+          info={backEndInfo.MongoDB.item}
         />
       </div>
     </section>
