ec84eecf44c01a699da4ec3a5d3e9fee4fbcd93d
Joshua <joshjoshuap1@gmail.com>
2022-07-13 15:39:41 +0800

    refractor: refractor links into seperate js file

---
+++ js/resources-info.js
@@ -0,0 +1,71 @@
+const resourcesInfo = [
+  {
+    frontEndResources: {
+      HTML: {
+        title: 'HTML',
+        item: [
+          { text: 'MDN Web HTML', link: 'https://developer.mozilla.org/en-US/docs/Web/HTML' },
+          { text: 'W3School HTML', link: 'https://www.w3schools.com/html/default.asp' },
+        ],
+      },
+      CSS: {
+        title: 'CSS',
+        item: [
+          { text: 'MDN Web CSS', link: 'https://developer.mozilla.org/en-US/docs/Web/CSS' },
+          { text: 'CSS Reference', link: 'https://cssreference.io/' },
+          { text: 'W3School CSS', link: 'https://www.w3schools.com/css/default.asp' },
+          { text: 'CSS Tricks', link: 'https://css-tricks.com/' },
+        ],
+      },
+      Javascript: {
+        title: 'Javascript',
+        item: [
+          {
+            text: 'MDN Web Javascript',
+            link: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript',
+          },
+          { text: 'Eloquent Javascript', link: 'https://eloquentjavascript.net/' },
+          { text: 'W3School Javascript', link: 'https://www.w3schools.com/js/default.asp' },
+          { text: 'Idiomatic Javascript', link: 'https://github.com/rwaldron/idiomatic.js' },
+        ],
+      },
+      Bootstrap: {
+        title: 'Bootstrap',
+        item: [
+          {
+            text: 'Bootstrap 5.2',
+            link: 'https://getbootstrap.com/docs/5.2/getting-started/introduction/',
+          },
+          { text: 'Bootsnipp Template', link: 'https://bootsnipp.com/' },
+          { text: 'Bootstrap Tools', link: 'https://hackerthemes.com/bootstrap-tools/' },
+          {
+            text: '  Bootstrap 5 Cheatsheet',
+            link: 'https://bootstrap-cheatsheet.themeselection.com/',
+          },
+        ],
+      },
+      TailwindCSS: {
+        title: 'Tailwind CSS',
+        item: [
+          { text: 'Tailwind CSS', link: 'https://tailwindcss.com/' },
+          { text: 'Tailwind Toolbox', link: 'https://www.tailwindtoolbox.com/tools' },
+          { text: 'Tailwind Components', link: 'https://postsrc.com/components' },
+          { text: 'Tailwind Tools', link: 'https://bestoftailwind.com/t/tools' },
+        ],
+      },
+      ReactJS: {
+        title: 'React JS',
+        item: [
+          { text: 'React JS', link: 'https://reactjs.org/docs/getting-started.html' },
+          { text: 'Material UI', link: 'https://mui.com/material-ui/getting-started/overview/' },
+          { text: 'Chakra UI', link: 'https://chakra-ui.com/getting-started' },
+          { text: 'Redux', link: 'https://redux.js.org/introduction/getting-started' },
+          { text: 'React Router', link: 'https://reactrouter.com/docs/en/v6' },
+          { text: 'ReactJS Cheatsheet', link: 'https://devhints.io/react' },
+        ],
+      },
+    },
+  },
+];
+
+export { resourcesInfo };
