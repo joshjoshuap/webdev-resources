d7663e4943de4f47445125d16f80ff85df9bdb8a
Joshua <joshjoshuap1@gmail.com>
2022-07-14 08:20:20 +0800

    chore: add comments, add meta width content

--- pages/sections/Frontend.js
+++ pages/sections/Frontend.js
@@ -9,28 +9,39 @@ import TailwindIcon from '/public/images/tailwindcss-icon.svg';
 import ReactIcon from '/public/images/reactjs-icon.svg';
 
 const FrontEnd = () => {
-  let frontEndInfo = resourcesInfo[0].frontEndResources;
+  let frontEndInfo = resourcesInfo[0].frontEndResources; // fetch the objects from resources-info,js
   return (
     <section id="frontend">
       <h1 className="section-title">FRONT END DEVELOPMENT</h1>
       <Card>
+        {/* HTML */}
         <CardItem image={HTMLIcon} title={frontEndInfo.HTML.title} info={frontEndInfo.HTML.item} />
+
+        {/* CSS */}
         <CardItem image={CSSIcon} title={frontEndInfo.CSS.title} info={frontEndInfo.CSS.item} />
+
+        {/* Javascript */}
         <CardItem
           image={JSIcon}
           title={frontEndInfo.Javascript.title}
           info={frontEndInfo.Javascript.item}
         />
+
+        {/* Bootstrap */}
         <CardItem
           image={BootstrapIcon}
           title={frontEndInfo.Bootstrap.title}
           info={frontEndInfo.Bootstrap.item}
         />
+
+        {/* TailwindCSS */}
         <CardItem
           image={TailwindIcon}
           title={frontEndInfo.TailwindCSS.title}
           info={frontEndInfo.TailwindCSS.item}
         />
+
+        {/* ReactJS */}
         <CardItem
           image={ReactIcon}
           title={frontEndInfo.ReactJS.title}
--- pages/sections/Fullstack.js
+++ pages/sections/Fullstack.js
@@ -5,16 +5,19 @@ import VsCodeIcon from '/public/images/vscode-icon.svg';
 import GitIcon from '/public/images/git-icon.svg';
 
 const FullStack = () => {
-  let fullStackInfo = resourcesInfo[0].fullStackResources;
+  let fullStackInfo = resourcesInfo[0].fullStackResources; // fetch the objects from resources-info,js
   return (
     <section id="fullstack">
       <h1 className="section-title">FULLSTACK DEVELOPMENT</h1>
       <Card>
+        {/* VSCode */}
         <CardItem
           image={VsCodeIcon}
           title={fullStackInfo.VSCode.title}
           info={fullStackInfo.VSCode.item}
         />
+
+        {/* Git */}
         <CardItem image={GitIcon} title={fullStackInfo.Git.title} info={fullStackInfo.Git.item} />
       </Card>
     </section>
