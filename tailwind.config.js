c00a45f7e91b27a08ca7805d1fda4450bf612cdb
Joshua <joshjoshuap1@gmail.com>
2023-09-02 17:27:55 +0800

    chore: configure colors and font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,28 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        black: "#040D12",
+        gray: "#526D82",
+        white: "#F8F9FC",
+        skywhite: "#EFF5F5",
+        darkblue: "#0E2954",
+        blue: "#0A6EBD",
+        lightblue: "#3A98B9",
+        darkorange: "#F97B22",
+        orange: "#F29727",
+        lightorange: "#FFB84C",
+        darksilver: "#C4DFDF",
+        silver: "#D2E9E9",
+        lightsilver: "#F4F9F9",
+      },
+    },
+    fontFamily: {
+      quicksand: ["Poppins", "sans-serif"],
+      dmmono: ["Roboto", "mono"],
+      robotoslab: ["Roboto Slab", "serif"],
+    },
   },
   plugins: [],
 };
