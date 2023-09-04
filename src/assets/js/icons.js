9519efe0fc96fe8ec5af96fbc048b2dcd56728fa
Joshua <joshjoshuap1@gmail.com>
2023-09-04 15:57:38 +0800

    chore: move js file

---
+++ src/assets/js/icons.js
@@ -0,0 +1,43 @@
+import apiIcon from "../icons/api-icon.png";
+import bootstrapIcon from "../icons/bootstrap-icon.svg";
+import codetoolsIcon from "../icons/codetools-icon.png";
+import colorsIcon from "../icons/colors-icon.png";
+import compilerIcon from "../icons/compiler-icon.png";
+import cssIcon from "../icons/css-icon.svg";
+import expressjsIcon from "../icons/expressjs-icon.svg";
+import githubIcon from "../icons/github-icon.svg";
+import gitIcon from "../icons/git-icon.svg";
+import htmlIcon from "../icons/html-icon.svg";
+import iconsIcon from "../icons/icons-icon.png";
+import imageIcon from "../icons/image-icon.png";
+import imagetoolsIcon from "../icons/imagetools-icon.png";
+import javascriptIcon from "../icons/javascript-icon.svg";
+import mongodbIcon from "../icons/mongodb-icon.svg";
+import nodejsIcon from "../icons/nodejs-icon.svg";
+import reactjsIcon from "../icons/reactjs-icon.svg";
+import tailwindIcon from "../icons/tailwind-icon.svg";
+import typographyIcon from "../icons/typography-icon.png";
+import vscodeIcon from "../icons/vscode-icon.svg";
+
+export {
+  apiIcon,
+  bootstrapIcon,
+  codetoolsIcon,
+  colorsIcon,
+  compilerIcon,
+  cssIcon,
+  expressjsIcon,
+  githubIcon,
+  gitIcon,
+  htmlIcon,
+  iconsIcon,
+  imageIcon,
+  imagetoolsIcon,
+  javascriptIcon,
+  mongodbIcon,
+  nodejsIcon,
+  reactjsIcon,
+  tailwindIcon,
+  typographyIcon,
+  vscodeIcon,
+};
